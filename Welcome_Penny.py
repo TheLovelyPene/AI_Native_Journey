c758a0ed64c2c75eb518aa931c32d4eb96a76ffa
TheLovelyPene <penelope.trent@pursuit.org>
2025-06-16 21:05:05 -0400

    Add files via upload

---
+++ Welcome_Penny.py
@@ -0,0 +1,7 @@
+# This line asks the user for their name and stores their input
+# in a variable called 'user_name'.
+user_name = input("What is your name? ")
+
+# This line then uses an f-string to create a personalized welcome message.
+# The value stored in 'user_name' will be inserted directly into the message.
+print(f"Welcome {user_name}! I am new to AI")
